5912ba6468c16f15c3aad3cf5e2fd6d589155207
Seunghyuk Choi <noaidi@gmail.com>
2024-04-09 07:08:28 +0900

    init

---
+++ assets/immutable/chunks/scent-memories._yIiY-U5.js
@@ -0,0 +1,10 @@
+import{s as c,n as p}from"./scheduler.WyyjKgw5.js";import{S as u,i as d,m as f,s as h,e as v,n as _,f as g,c as O,q as b,r as A,o as K,g as a,k as S,l as $,d as l,p as w}from"./index.7WYNH0e2.js";import{P as M}from"./PageTitle.rYtw2zuh.js";function x(m){let t,s,n,r=`<div class="grid-2cols"><div>During the summer of 2023 over the course of three months, KOO JEONG
+			A collected scent memories for <em>ODORAMA CITIES</em> with the aim of
+			building a portrait of the entire Korean peninsula. After a public open
+			call issued by KOO and the pavilion curatorial team via social media,
+			advertisements, one-to-one meetings, Koreans, non-Koreans, and other
+			publics with a relationship to Korea submitted descriptions in response
+			to one central question: “What is your scent memory of Korea?”</div> <div><strong>All scent memories collected during June 25 – September 30,
+				2023 will be available here from April 17, 2024.</strong><br/> <br/>
+			A new version of KOO JEONG A’s exhibition <em>ODORAMA CITIES</em> will
+			open at ARKO Art Center, Seoul, from December 20, 2024 to March 23, 2025.</div></div>`,o;return t=new M({props:{title:"Scent Memories"}}),{c(){f(t.$$.fragment),s=h(),n=v("div"),n.innerHTML=r,this.h()},l(e){_(t.$$.fragment,e),s=g(e),n=O(e,"DIV",{class:!0,"data-svelte-h":!0}),b(n)!=="svelte-13sn6tc"&&(n.innerHTML=r),this.h()},h(){A(n,"class","container")},m(e,i){K(t,e,i),a(e,s,i),a(e,n,i),o=!0},p,i(e){o||(S(t.$$.fragment,e),o=!0)},o(e){$(t.$$.fragment,e),o=!1},d(e){e&&(l(s),l(n)),w(t,e)}}}class I extends u{constructor(t){super(),d(this,t,null,x,c,{})}}export{I as S};
---
+++ assets/immutable/entry/start.CV0F2XKx.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.JBLhtQxi.js";export{t as start};
