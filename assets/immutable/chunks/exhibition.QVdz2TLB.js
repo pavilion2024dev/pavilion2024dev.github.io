20b16bb48559468dfc6b897308d71dd334ad8ed8
Seunghyuk Choi <noaidi@gmail.com>
2024-04-15 01:17:38 +0900

    information

---
+++ assets/immutable/chunks/exhibition.QVdz2TLB.js
@@ -0,0 +1,127 @@
+import{s as B,c as P}from"./scheduler.1cGUQF-q.js";import{S as R,i as C,m as D,s as _,e as u,n as N,f as O,c as f,a as x,d as s,r as T,o as z,g as l,h as E,k as V,l as W,p as q,t as v,b as w,q as A}from"./index.1AxmEcE_.js";import{P as G}from"./PageTitle.RRRWWl6V.js";import{l as J}from"./route.ePqwXtlT.js";function M(p){let i,o,n,a,r,t,c="ODORAMA CITIES",m;return{c(){i=v(`KOO JEONG A (they/them) is constantly in orbit, living and
+				working everywhere. In their practice, architectural elements,
+				texts, drawings, paintings, sculptures, animations, sound, film,
+				words, and scents play a significant role. Throughout the years,
+				KOO has investigated and blurred the lines between their artwork
+				and the space it occupies. The works add new layers to any given
+				space, and KOO manages to merge small intimate experiences and
+				large-scale immersive pieces.`),o=u("br"),n=_(),a=u("br"),r=v(`
+				The curatorial approach for the Korean Pavilion at the 60th International
+				Art Exhibition – La Biennale di Venezia has been to combine some
+				of the key subjects and sculptural elements that KOO JEONG A has
+				worked with during the last three decades. With the new commission
+				`),t=u("em"),t.textContent=c,m=v(`, created especially for the Korean
+				Pavilion, KOO delves into the nuances of our spatial encounters,
+				investigating how we perceive and recollect spaces, with a
+				particular emphasis on how scents, smells, and odors contribute
+				to these memories. With the pavilion itself, KOO explores an
+				expanded tactility.`)},l(e){i=w(e,`KOO JEONG A (they/them) is constantly in orbit, living and
+				working everywhere. In their practice, architectural elements,
+				texts, drawings, paintings, sculptures, animations, sound, film,
+				words, and scents play a significant role. Throughout the years,
+				KOO has investigated and blurred the lines between their artwork
+				and the space it occupies. The works add new layers to any given
+				space, and KOO manages to merge small intimate experiences and
+				large-scale immersive pieces.`),o=f(e,"BR",{}),n=O(e),a=f(e,"BR",{}),r=w(e,`
+				The curatorial approach for the Korean Pavilion at the 60th International
+				Art Exhibition – La Biennale di Venezia has been to combine some
+				of the key subjects and sculptural elements that KOO JEONG A has
+				worked with during the last three decades. With the new commission
+				`),t=f(e,"EM",{"data-svelte-h":!0}),A(t)!=="svelte-1usdt3w"&&(t.textContent=c),m=w(e,`, created especially for the Korean
+				Pavilion, KOO delves into the nuances of our spatial encounters,
+				investigating how we perceive and recollect spaces, with a
+				particular emphasis on how scents, smells, and odors contribute
+				to these memories. With the pavilion itself, KOO explores an
+				expanded tactility.`)},m(e,d){l(e,i,d),l(e,o,d),l(e,n,d),l(e,a,d),l(e,r,d),l(e,t,d),l(e,m,d)},d(e){e&&(s(i),s(o),s(n),s(a),s(r),s(t),s(m))}}}function j(p){let i,o,n,a,r;return{c(){i=v(`구정아는 모든 곳에서 살고 일하며 언제나 궤도를 돌고 있다. 그런
+				그의 작업에서 건축 요소, 글, 드로잉, 그림, 조각, 애니메이션,
+				사운드, 영상, 단어, 향은 중차대한 자리를 차지한다. 그간의
+				활동에서 구정아는 작품과 공간의 경계를 탐구하며 그 구분을
+				흐려왔다. 공간마다 새로운 겹을 더하는 작품으로 소소하고 내밀한
+				경험과 대규모의 몰입형 작업을 융합해 낸다.`),o=u("br"),n=_(),a=u("br"),r=v(`
+				베니스비엔날레 제60회 국제미술전 한국관 전시는 구정아 작가가 지난
+				30여 년간 다루어 온 주요 주제와 특유의 조각적 측면들을 아우른다.
+				작가의 광범위한 접근 가운데 반복적으로 등장하는 테마는 ‘향’이다.
+				한국관을 위해 새로이 제작된 «오도라마 시티»를 통해 구정아는 공간적
+				조우의 다양한 뉘앙스를 살피고, 냄새와 향기가 기억에 어떻게 작용하는지에
+				집중하며 우리가 공간을 감지하고 회상하는 방식을 탐구한다. 그리고
+				확장된 촉감을 공간에 걸쳐 연구한다.`)},l(t){i=w(t,`구정아는 모든 곳에서 살고 일하며 언제나 궤도를 돌고 있다. 그런
+				그의 작업에서 건축 요소, 글, 드로잉, 그림, 조각, 애니메이션,
+				사운드, 영상, 단어, 향은 중차대한 자리를 차지한다. 그간의
+				활동에서 구정아는 작품과 공간의 경계를 탐구하며 그 구분을
+				흐려왔다. 공간마다 새로운 겹을 더하는 작품으로 소소하고 내밀한
+				경험과 대규모의 몰입형 작업을 융합해 낸다.`),o=f(t,"BR",{}),n=O(t),a=f(t,"BR",{}),r=w(t,`
+				베니스비엔날레 제60회 국제미술전 한국관 전시는 구정아 작가가 지난
+				30여 년간 다루어 온 주요 주제와 특유의 조각적 측면들을 아우른다.
+				작가의 광범위한 접근 가운데 반복적으로 등장하는 테마는 ‘향’이다.
+				한국관을 위해 새로이 제작된 «오도라마 시티»를 통해 구정아는 공간적
+				조우의 다양한 뉘앙스를 살피고, 냄새와 향기가 기억에 어떻게 작용하는지에
+				집중하며 우리가 공간을 감지하고 회상하는 방식을 탐구한다. 그리고
+				확장된 촉감을 공간에 걸쳐 연구한다.`)},m(t,c){l(t,i,c),l(t,o,c),l(t,n,c),l(t,a,c),l(t,r,c)},d(t){t&&(s(i),s(o),s(n),s(a),s(r))}}}function L(p){let i,o,n,a,r,t,c="ODORAMA CITIES",m;return{c(){i=v(`Some of the prominent interests in KOO’s art, such as
+				immaterialism, weightlessness, endlessness, and levitation, are
+				keywords mirrored throughout the Korean Pavilion. They are
+				embedded and engraved as infinity symbols directly into both the
+				new wooden floor and the outdoor installations, are manifested
+				as two floating wooden möbius-shaped sculptures and a
+				levitating, scent-diffusing bronze figure, and finally are
+				symbolized in the scents that transform the pavilion into a
+				collection of olfactory memories.`),o=u("br"),n=_(),a=u("br"),r=v(`
+				These scent memories are a cornerstone in
+				`),t=u("em"),t.textContent=c,m=v(`. During the summer of 2023, KOO
+				collected them with the aim of making a scent portrait of the
+				Korean peninsula. Through social media, advertisements, press
+				releases, and personal one-on-one meetings, the team behind the
+				Korean Pavilion has reached out to North and South Koreans and
+				non-Koreans alike – anyone who has a relationship to Korea – and
+				asked the question: “What is your scent memory of Korea?” This
+				open call has generated more than 600 written statements about
+				Korean scents. The perfumers, armed with the stories and
+				keywords, took on the task of interpreting and incorporating
+				them into the creation of 16 distinct scent experiences for the
+				pavilion and a single commercial fragrance.`)},l(e){i=w(e,`Some of the prominent interests in KOO’s art, such as
+				immaterialism, weightlessness, endlessness, and levitation, are
+				keywords mirrored throughout the Korean Pavilion. They are
+				embedded and engraved as infinity symbols directly into both the
+				new wooden floor and the outdoor installations, are manifested
+				as two floating wooden möbius-shaped sculptures and a
+				levitating, scent-diffusing bronze figure, and finally are
+				symbolized in the scents that transform the pavilion into a
+				collection of olfactory memories.`),o=f(e,"BR",{}),n=O(e),a=f(e,"BR",{}),r=w(e,`
+				These scent memories are a cornerstone in
+				`),t=f(e,"EM",{"data-svelte-h":!0}),A(t)!=="svelte-1usdt3w"&&(t.textContent=c),m=w(e,`. During the summer of 2023, KOO
+				collected them with the aim of making a scent portrait of the
+				Korean peninsula. Through social media, advertisements, press
+				releases, and personal one-on-one meetings, the team behind the
+				Korean Pavilion has reached out to North and South Koreans and
+				non-Koreans alike – anyone who has a relationship to Korea – and
+				asked the question: “What is your scent memory of Korea?” This
+				open call has generated more than 600 written statements about
+				Korean scents. The perfumers, armed with the stories and
+				keywords, took on the task of interpreting and incorporating
+				them into the creation of 16 distinct scent experiences for the
+				pavilion and a single commercial fragrance.`)},m(e,d){l(e,i,d),l(e,o,d),l(e,n,d),l(e,a,d),l(e,r,d),l(e,t,d),l(e,m,d)},d(e){e&&(s(i),s(o),s(n),s(a),s(r),s(t),s(m))}}}function F(p){let i,o,n,a,r;return{c(){i=v(`향의 본질을 탐구하고 분자를 들이쉬고 내쉬는 과정에 대한 작가의
+				관심은 비물질주의, 무중력, 무한, 공중부양이라는 주제로 연결된다.
+				이는 올해 한국관 곳곳에 반영된 테마이자 열쇠 말들이기도 하다.
+				전시장 바닥에 새긴 무한대 기호로서, 뫼비우스의 띠 형태로
+				부유하는 두 개의 나무 조각으로서, 또한 향을 퍼뜨리는 디퓨저이자
+				공중에 띄운 동상으로서, 아울러 이 공간을 ‘향기 메모리’의
+				집합체로 변모시킬 향과 냄새가 한국관을 관통하며 변주한다.`),o=u("br"),n=_(),a=u("br"),r=v(`
+				2023년 여름, 구정아는 한반도의 향기 초상을 그리려 «오도라마 시티»를
+				위한 ‘향기 메모리’를 수집했다. 한국관 전시 팀은 SNS와 광고, 언론
+				보도와 개인적인 면담 등을 통해 남한과 북한 사람 및 비한국인 – 곧
+				한반도와 연이 있는 모두 – 에게 ‘한국(코리아)의 도시, 고향에 얽힌
+				향의 기억’에 대해 물었다. 이 오픈 콜을 통해 600편이 넘는 글이 모였고,
+				조향사들은 사연과 주제어를 바탕으로 한국관에 16개의 상이한 냄새 경험을
+				조성해 줄 향들과 하나의 상업 향수를 만들었다.`)},l(t){i=w(t,`향의 본질을 탐구하고 분자를 들이쉬고 내쉬는 과정에 대한 작가의
+				관심은 비물질주의, 무중력, 무한, 공중부양이라는 주제로 연결된다.
+				이는 올해 한국관 곳곳에 반영된 테마이자 열쇠 말들이기도 하다.
+				전시장 바닥에 새긴 무한대 기호로서, 뫼비우스의 띠 형태로
+				부유하는 두 개의 나무 조각으로서, 또한 향을 퍼뜨리는 디퓨저이자
+				공중에 띄운 동상으로서, 아울러 이 공간을 ‘향기 메모리’의
+				집합체로 변모시킬 향과 냄새가 한국관을 관통하며 변주한다.`),o=f(t,"BR",{}),n=O(t),a=f(t,"BR",{}),r=w(t,`
+				2023년 여름, 구정아는 한반도의 향기 초상을 그리려 «오도라마 시티»를
+				위한 ‘향기 메모리’를 수집했다. 한국관 전시 팀은 SNS와 광고, 언론
+				보도와 개인적인 면담 등을 통해 남한과 북한 사람 및 비한국인 – 곧
+				한반도와 연이 있는 모두 – 에게 ‘한국(코리아)의 도시, 고향에 얽힌
+				향의 기억’에 대해 물었다. 이 오픈 콜을 통해 600편이 넘는 글이 모였고,
+				조향사들은 사연과 주제어를 바탕으로 한국관에 16개의 상이한 냄새 경험을
+				조성해 줄 향들과 하나의 상업 향수를 만들었다.`)},m(t,c){l(t,i,c),l(t,o,c),l(t,n,c),l(t,a,c),l(t,r,c)},d(t){t&&(s(i),s(o),s(n),s(a),s(r))}}}function H(p){let i,o,n,a,r,t,c,m;i=new G({props:{title:"Exhibition"}});function e(h,b){return h[0]==="ko"?j:M}let d=e(p),y=d(p);function I(h,b){return h[0]==="ko"?F:L}let k=I(p),g=k(p);return{c(){D(i.$$.fragment),o=_(),n=u("div"),a=u("div"),r=u("div"),y.c(),t=_(),c=u("div"),g.c(),this.h()},l(h){N(i.$$.fragment,h),o=O(h),n=f(h,"DIV",{class:!0,"data-lang":!0});var b=x(n);a=f(b,"DIV",{class:!0});var K=x(a);r=f(K,"DIV",{});var S=x(r);y.l(S),S.forEach(s),t=O(K),c=f(K,"DIV",{});var $=x(c);g.l($),$.forEach(s),K.forEach(s),b.forEach(s),this.h()},h(){T(a,"class","grid-2cols"),T(n,"class","container"),T(n,"data-lang",p[0])},m(h,b){z(i,h,b),l(h,o,b),l(h,n,b),E(n,a),E(a,r),y.m(r,null),E(a,t),E(a,c),g.m(c,null),m=!0},p(h,[b]){d!==(d=e(h))&&(y.d(1),y=d(h),y&&(y.c(),y.m(r,null))),k!==(k=I(h))&&(g.d(1),g=k(h),g&&(g.c(),g.m(c,null))),(!m||b&1)&&T(n,"data-lang",h[0])},i(h){m||(V(i.$$.fragment,h),m=!0)},o(h){W(i.$$.fragment,h),m=!1},d(h){h&&(s(o),s(n)),q(i,h),y.d(),g.d()}}}function Q(p,i,o){let n;return P(p,J,a=>o(0,n=a)),[n]}class tt extends R{constructor(i){super(),C(this,i,Q,H,B,{})}}export{tt as E};
---
+++ assets/immutable/entry/start.-pA0guCK.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.8v34G7Ep.js";export{t as start};
